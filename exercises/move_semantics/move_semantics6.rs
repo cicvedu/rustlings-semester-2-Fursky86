3102d6391bf336bde17ba1666bdd2d6540956c21
Fursky86 <jiangxiaoyang_dec@163.com>
2023-11-03 13:18:59 +0800

    update

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,24 +5,25 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    let last_char = get_char(&data);  // change to borrow instead of moving
+    println!("Last character is: {}", last_char);
 
-    string_uppercase(&data);
+    string_uppercase(data);  // transfer ownership
 }
 
-// Should not take ownership
-fn get_char(data: String) -> char {
+// Does not take ownership, uses a reference
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
-// Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
-
+// Takes ownership
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
     println!("{}", data);
 }
+
